7d4c900e46b2dcd49fcc42b1f7a8da6b1331c1b2
DomagojPavlovic <crohawk004@gmail.com>
2025-06-24 17:45:47 +0200

    random
    - added random.h from learncpp.com
    - implemented a few base random functions

---
+++ src/random/randomUtils.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "random.h"
+#include "enemyNames.h"
+#include "enemyValues.h"
+#include "enemy.h"
+
+namespace RandomUtils {
+
+    Enemy generateRandomEnemy(int level, bool isBigEnemy = false) {
+        if (level < 1 || level > EnemyValues::maxEnemyLevel) {
+            throw std::runtime_error("Invalid enemy level.");
+        }
+
+        return { 
+            level, 
+            EnemyValues::getRandomEnemyValueForLevel(EnemyValues::hp, level), 
+            EnemyValues::getRandomEnemyValueForLevel(EnemyValues::armor, level), 
+            EnemyValues::getRandomEnemyValueForLevel(EnemyValues::resistance, level),  
+            EnemyValues::getRandomEnemyValueForLevel(EnemyValues::blockChance, level), 
+            EnemyValues::getRandomEnemyValueForLevel(EnemyValues::strength, level),
+            EnemyValues::getRandomEnemyValueForLevel(EnemyValues::intelligence, level),
+            EnemyValues::getRandomEnemyValueForLevel(EnemyValues::gold, level),
+            EnemyNames::getRandomEnemyName(isBigEnemy)
+        };
+    }
+
+    bool executePercetageChance(int percentage) {
+        if (percentage < 0 || percentage > 100) {
+            throw std::runtime_error("Percentage must be between 0 and 100.");
+        }
+        return Random::get(1, 100) <= percentage;
+    }
+
+}
