d2b1964aea326e673446836a57a0e8f1baacd1a0
DomagojPavlovic <crohawk004@gmail.com>
2025-06-24 17:48:18 +0200

    game
    - pointer getters for ui

--- src/game/game.h
+++ src/game/game.h
@@ -11,11 +11,24 @@ public:
 
     Game() = default;
 
+    Player* getPointerToPlayer() { return &m_player; }
+
+    std::vector<Enemy>* getPointerToEnemies() { return &m_enemies; }
+
+    Location* getPointerToLocation() { return &m_location; }
+
+    void addEnemy(const Enemy& enemy) {
+        m_enemies.push_back(enemy);
+    }
+
+    void removeAllEnemies() {
+        m_enemies.clear();
+    }
+
 private:
 
     Player              m_player {};
     std::vector<Enemy>  m_enemies {};
     Location            m_location {};
-    
 
 };
